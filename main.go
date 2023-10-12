7ce3d3f31378124472a313fef12a7df3e87ce1da
khang nguyen <keirames.l3@gmail.com>
2023-10-12 20:36:02 +0700

    init

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
+type Server struct {
+}
+
+type ConsensusModule struct {
+	// a server ID of this CM
+	id int
+
+	// the IDs of our peers in the cluster
+	peerIDs []int
+
+	// the server containing this CM. It's used to issue RPC calls to peers.
+	server *Server
+}
+
 func main() {}
